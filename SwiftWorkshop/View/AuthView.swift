b7f78d18d1c646b071f53392a03d2afb58845d6f
Bernardo <bernardosantiago44@gmail.com>
2024-12-03 13:18:40 -0600

    Authentication UI & Logic.

--- SwiftWorkshop/View/AuthView.swift
+++ SwiftWorkshop/View/AuthView.swift
@@ -8,22 +8,59 @@
 import SwiftUI
 
 struct AuthView: View {
-    @State var email = ""
-    @State var password = ""
+    @State var authViewModel = AuthViewModel()
     @FocusState private var focus: String?
     
     var body: some View {
         VStack {
-            TextField("Email", text: $email)
+            TextField("Email", text: $authViewModel.email)
                 .textFieldStyle(RoundedTextFieldStyle())
                 .focused($focus, equals: "emailField")
                 .textContentType(.emailAddress)
                 .keyboardType(.emailAddress)
             
-            SecureField("Contraseña", text: $password)
+            SecureField("Contraseña", text: $authViewModel.password)
                 .textFieldStyle(RoundedTextFieldStyle())
+            
+            ViewThatFits {
+                HStack {
+                    registerButton()
+                    loginButton()
+                }
+                .disabled(self.authViewModel.email.isEmpty || self.authViewModel.password.isEmpty)
+                
+                VStack {
+                    registerButton()
+                    loginButton()
+                }
+                .disabled(self.authViewModel.email.isEmpty || self.authViewModel.password.isEmpty)
+            }
         }
         .padding()
+    }
+    
+    @ViewBuilder private func registerButton() -> some View {
+        Button {
+            Task {
+                await self.authViewModel.registerUser()
+            }
+        } label: {
+            Text("Registrarse")
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.bordered)
+    }
+    
+    @ViewBuilder private func loginButton() -> some View {
+        Button {
+            Task {
+                await self.authViewModel.loginWithEmailPassword()
+            }
+        } label: {
+            Text("Iniciar sesion")
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
     }
 }
 
